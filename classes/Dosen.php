9d74a17cc11327042f0879345f5a5a20e3a42888
keisyarosintamanalu <keisyarosintamanalu@gmail.com>
2026-04-08 17:02:45 +0700

    done

--- classes/Dosen.php
+++ classes/Dosen.php
@@ -1,12 +1,11 @@
-<?php
-require_once __DIR__ . '/User.php';
-
 class Dosen extends User {
     private string $nip;
+
     public function __construct(string $nip, string $nama) {
-        parent::__construct($nip, $nama, 'dosen');
+        parent::__construct($nip, $nama, 'dosen'); // sudah benar
         $this->nip = $nip;
     }
+
     public function getInfo(): string {
         return "Dosen: {$this->nama} | NIP: {$this->nip}";
     }
